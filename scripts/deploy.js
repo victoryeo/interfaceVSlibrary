ca336fdc91936cf5bd6f96488f9216c1c7758ce1
yeosiowvic <victoryeo1974@gmail.com>
2021-08-25 09:26:09 +0800

    add StorageContract and StorageLib to deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,10 +6,23 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  const con1 = await ethers.getContractFactory("ActionWithLib");
+  const ConSto = await ethers.getContractFactory("StorageContract");
+  //console.log(Lib)
   console.log('Deploying ...');
-  const con1 = await Con1.deploy();
-  console.log("Contract address:", con1.address);
+  const conSto = await ConSto.deploy();
+  console.log("StorageContract address:", conSto.address);
+
+  const Lib = await ethers.getContractFactory("StorageLib");
+  //console.log(Lib)
+  console.log('Deploying ...');
+  const lib = await Lib.deploy();
+  console.log("lib address:", lib.address);
+
+  const ConAction = await ethers.getContractFactory("ActionWithLib", {libraries:{StorageLib: lib.address}});
+  //console.log(ConAction)
+  console.log('Deploying ...');
+  const conAc = await ConAction.deploy(conSto.address);
+  console.log("ActionWithLib address:", conAc.address);
 }
 
 main()
