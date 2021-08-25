f95afff3e021b4c9c5c6067a13627765e064dc29
yeosiowvic <victoryeo1974@gmail.com>
2021-08-25 09:37:55 +0800

    add test file to test smart contract

--- test/library.test.js
+++ test/library.test.js
@@ -2,13 +2,34 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Test contract", function () {
-  it("Deployment", async function () {
+  let conAc;
+
+  beforeEach(async function () {
     const [owner] = await ethers.getSigners();
 
-    const Con1 = await ethers.getContractFactory("ActionWithLib");
+    const ConSto = await ethers.getContractFactory("StorageContract");
+    //console.log(Lib)
+    console.log('Deploying ...');
+    const conSto = await ConSto.deploy();
+    console.log("StorageContract address:", conSto.address);
+  
+    const Lib = await ethers.getContractFactory("StorageLib");
+    //console.log(Lib)
+    console.log('Deploying ...');
+    const lib = await Lib.deploy();
+    console.log("Library address:", lib.address);
+  
+    const ConAction = await ethers.getContractFactory("ActionWithLib", {libraries:{StorageLib: lib.address}});
+    //console.log(ConAction)
+    console.log('Deploying ...');
+    conAc = await ConAction.deploy(conSto.address);
+    console.log("ActionWithLib address:", conAc.address);
+  });
 
-    const con1 = await Con1.deploy();
-
+  it('retrieve same value', async function () {
+    let returnVal = await conAc.doSth();
+    console.log(returnVal.toString())
+    expect(returnVal.toString()).to.equal("2");
   });
 
 });
