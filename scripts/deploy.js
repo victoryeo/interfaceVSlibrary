03d0232c26cd35b9c96f762d976cd1199b3657ed
yeosiowvic <victoryeo1974@gmail.com>
2021-08-25 15:24:17 +0800

    add interface contract deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,6 +6,7 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
+  // deploy library
   const ConSto = await ethers.getContractFactory("StorageContract");
   console.log('Deploying ...');
   const conSto = await ConSto.deploy();
@@ -22,6 +23,18 @@ async function main() {
   console.log('Deploying ...');
   const conAc = await ConAction.deploy(conSto.address);
   console.log("ActionWithLib address:", conAc.address);
+
+  // deploy interface
+  const ConStoInt = await ethers.getContractFactory("StorageUsingInterface");
+  console.log('Deploying ...');
+  const conStoInt = await ConStoInt.deploy();
+  console.log("StorageUsingInterface address:", conStoInt.address);
+
+  const ConInt = await ethers.getContractFactory("ActionWithInterface");
+  //console.log(ConInt)
+  console.log('Deploying ...');
+  conInt = await ConInt.deploy(conSto.address);
+  console.log("ActionWithInterface address:", conInt.address);
 }
 
 main()
